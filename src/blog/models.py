9d026ceb03efc47de46fe26287056f6e2b033654
Alex <smirnov1991alexx@gmail.com>
2023-03-10 17:55:33 +0500

    step 10 (pagination)

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(max_length=255, verbose_name='URL', unique=True)
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"slug": self.slug})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Наименование')
+    slug = models.SlugField(max_length=50, verbose_name='URL', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, verbose_name='URL', unique=True)
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    content = models.TextField(blank=True, verbose_name='Содержание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Изображение')
+    views = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='Тэги', related_name='posts')
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={"slug": self.slug})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+        ordering = ['-created_at']
--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('category/<str:slug>/', get_category, name='category'),
+    path('post/<str:slug>/', get_post, name='post'),
+    path('', Home.as_view(), name='index'),
 ]
